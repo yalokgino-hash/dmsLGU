00d77cdb00a68273559d93719639804da90b7dc5
yalokgino-hash <yalokgino@gmail.com>
2026-02-17 19:54:33 -0800

    admin side

--- Admin Side/admin_dashboard.php
+++ Admin Side/admin_dashboard.php
@@ -106,7 +106,7 @@ $userName = $_SESSION['user_name'] ?? $_SESSION['user_email'] ?? 'Admin';
                             </a>
                             <a href="admin_offices.php" class="quick-action-btn quick-action-offices">
                                 <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><path d="M9 22V12h6v10"/></svg>
-                                Office Codes
+                                Offices
                             </a>
                             <a href="documents.php" class="quick-action-btn quick-action-property">
                                 <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><path d="M14 2v6h6"/><path d="M16 13H8"/><path d="M16 17H8"/><path d="M10 9H8"/></svg>
